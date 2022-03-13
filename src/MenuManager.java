948b0e1167e970c96c30988a7671a1bf3e5517b7
inchang4043 <dlsckddld@naver.com>
2022-03-13 21:11:08 +0900

    added InChangManagementSystem.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add hobby");
+		System.out.println("2. Delete hobby");
+		System.out.println("3. Edit hobby");
+		System.out.println("4. View hobby");
+		System.out.println("5. Exit");
+	}
+
+}
